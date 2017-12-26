89ccb9fb784d0cfee3a1067d6484e9fdeb0134d3
Christian Tschenett <chtz@me.com>
2017-12-26 22:16:06 +0100

    PMW-compatibility-hacks. long-polling hacks. even more hacks ;-)

--- src/main/java/ch/furthermore/pmq/QueueController.java
+++ src/main/java/ch/furthermore/pmq/QueueController.java
@@ -1,7 +1,9 @@
 package ch.furthermore.pmq;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
@@ -21,6 +24,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * 
  * $ curl https://pmq.furthermore.ch/queues/fb1b23a9-00ef-4032-a68e-9159adeb65e8
  * ["Halli","Hallo"]
+ * </pre>
+ * 
+ * <pre>
+ * $ curl -X POST -H "Content-Type: application/json" -d "[]" http://localhost:4040/queues
+ * cef6c757-9035-44c2-85bb-858be5fb11b8
+ * 
+ * $ curl -X POST -H "Content-Type: application/json" -d '{"item":"hallo"}' http://localhost:4040/queues/cef6c757-9035-44c2-85bb-858be5fb11b8
+ * {"count":"1"}
+ * 
+ * $ curl http://localhost:4040/queues/cef6c757-9035-44c2-85bb-858be5fb11b8?poll=long
+ * ["hallo"]
  * </pre>
  */
 @Controller
@@ -41,7 +55,7 @@ public class QueueController {
 	
 	@RequestMapping(path="/queues/{queueId}", method=RequestMethod.PUT, consumes="application/json", produces="application/json")
 	@ResponseBody
-	public String addToQueue(@RequestBody List<String> data, @PathVariable("queueId") String queueId) {
+	public String addItemsToQueue(@RequestBody List<String> data, @PathVariable("queueId") String queueId) {
 		try {
 			LinkedList<String> queue = queueDAO.load(queueId);
 			queue.addAll(data);
@@ -52,13 +66,47 @@ public class QueueController {
 		}
 	}
 	
+	@RequestMapping(path="/queues/{queueId}", method=RequestMethod.POST, consumes="application/json", produces="application/json")
+	@ResponseBody
+	@Deprecated //for PMW compatibility: maps as in- and out params
+	public Map<String,String> addItemToQueue(@RequestBody Map<String,String> request, @PathVariable("queueId") String queueId) { //FIXME atomic
+		try {
+			String item = request.get("item");
+			
+			LinkedList<String> queue = queueDAO.load(queueId);
+			queue.add(item);
+			
+			queueDAO.save(queueId, queue);
+			
+			Map<String,String> response = new HashMap<String,String>();
+			response.put("count", Integer.toString(queue.size()));
+			return response;
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
 	@RequestMapping(path="/queues/{queueId}", method=RequestMethod.GET, produces="application/json") //FIXME idempotent GET
 	@ResponseBody
-	public List<String> flushQueue(@PathVariable("queueId") String queueId) {
+	public List<String> getAndRemoveAllItemsFromQueue(@PathVariable("queueId") String queueId, @RequestParam(name="poll", defaultValue="short") String poll) {
+		return getAndRemoveAllItemsFromQueue(queueId, "long".equals(poll)
+				? System.currentTimeMillis() + 10 * 1000
+				: System.currentTimeMillis() - 1
+				);
+	}
+	
+	public List<String> getAndRemoveAllItemsFromQueue(String queueId, long timeout) { //FIXME atomic
 		try {
 			LinkedList<String> queue = queueDAO.load(queueId);
-			queueDAO.save(queueId, new LinkedList<String>());
-			return queue;
+			if (queue.isEmpty() && System.currentTimeMillis() < timeout) {
+				Thread.sleep(1000); //FIXME don't waste resources (threads) - use async servlet & timer (or async proto like websockets or ...)
+				return getAndRemoveAllItemsFromQueue(queueId, timeout);
+			}
+			else {
+				queueDAO.save(queueId, new LinkedList<String>());
+				return queue;
+			}
 		}
 		catch (Exception e) {
 			throw new RuntimeException(e);
--- src/main/java/ch/furthermore/pmq/QueueDAO.java
+++ src/main/java/ch/furthermore/pmq/QueueDAO.java
@@ -28,12 +28,12 @@ public class QueueDAO {
 	}
 
 	@SuppressWarnings("unchecked")
-	public LinkedList<String> load(String id) throws IOException {
+	public synchronized LinkedList<String> load(String id) throws IOException { //FIXME lock file, not DAO ;-)
 		File queueFile = new File(storage, UUID.fromString(id).toString() + ".json");
 		return om.readValue(queueFile, LinkedList.class);
 	}
 	
-	public String save(String id, List<String> data) throws IOException {
+	public synchronized String save(String id, List<String> data) throws IOException {
 		id = id == null ? UUID.randomUUID().toString() : UUID.fromString(id).toString();
 		File queueFile = new File(storage, id + ".json");
 		om.writeValue(queueFile, data);
